f7bed81cf716508936c2a2475df424a580380d0e
Chucho0 <anonimobuscando666@outlook.com>
2023-10-05 02:57:31 -0500

    validacion

--- main.js
+++ main.js
@@ -1,12 +1,68 @@
-function enviar() {
+function validar() {
     let nombre = document.getElementById("minombre").value;
     let apellido = document.getElementById("miapellido").value;
-    let tipoDocumento = document.querySelector('input[name="identificacion"]:checked').value;
+    let tipoDocumentoElement = document.querySelector('input[name="identificacion"]:checked');
+    let tipoDocumento = tipoDocumentoElement ? tipoDocumentoElement.value : null;
     let numeroDocumento = document.getElementById("numeroidentificacion").value;
-    let genero = document.querySelector('input[name="genero"]:checked').value;
-    let fechaNacimiento = document.getElementById("fechadenacimiento").value;
+    let generoElement = document.querySelector('input[name="genero"]:checked');
+    let genero = generoElement ? generoElement.value : null;
+    let fecha = document.getElementById("fecha").value;
     let telefono = document.getElementById("numerodetelefono").value;
     let correo = document.getElementById("correoeletronico").value;
+
+    if (nombre === "") {
+        mostrarAlerta("Ingrese su nombre");
+        return;
+    }
+    if (apellido === "") {
+        mostrarAlerta("Ingrese su apellido");
+        return;
+    }
+    if (tipoDocumento === null) {
+        mostrarAlerta("Seleccione el tipo de documento");
+        return;
+    }
+    if (numeroDocumento === "") {
+        mostrarAlerta("Ingrese el número de documento");
+        return;
+    }
+    if (fecha === "") {
+        mostrarAlerta("Ingrese la fecha de nacimiento");
+        return;
+    } else {
+        let fechaNacimiento = new Date(fecha);
+        let hoy = new Date();
+        let edad = hoy.getFullYear() - fechaNacimiento.getFullYear();
+        let mes = hoy.getMonth() - fechaNacimiento.getMonth();
+        if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNacimiento.getDate())) {
+            edad--;
+        }
+        if (edad < 15) {
+            mostrarAlerta("Debe ser mayor de 15 años para registrarse.");
+            return;
+        }
+    }
+    if (genero === null) {
+        mostrarAlerta("Seleccione su genero");
+        return;
+    }
+    if (telefono === "") {
+        mostrarAlerta("Ingrese su número de teléfono");
+        return;
+    } else if (telefono.length !== 10 || isNaN(telefono)) {
+        mostrarAlerta("El número de teléfono debe tener 10 dígitos.");
+        return;
+    }
+    if (correo === "") {
+        mostrarAlerta("Ingrese una dirección de correo electronico");
+        return;
+    } else {
+        let correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!correoRegex.test(correo)) {
+            mostrarAlerta("introduzca una dirección de correo válida.");
+            return;
+        }
+    }
 
     let persona = {
         nombre: nombre,
@@ -14,14 +70,33 @@ function enviar() {
         tipoDocumento: tipoDocumento,
         numeroDocumento: numeroDocumento,
         genero: genero,
-        fechaNacimiento: fechaNacimiento,
+        fechaNacimiento: fecha,
         telefono: telefono,
         correo: correo,
     };
 
-    let informacion=[];
-
+    let informacion = [];
     informacion.push(persona);
 
-    alert("Información guardada con exito")
+    document.getElementById("minombre").value = "";
+    document.getElementById("miapellido").value = "";
+    if (tipoDocumentoElement) tipoDocumentoElement.checked = false;
+    document.getElementById("numeroidentificacion").value = "";
+    if (generoElement) generoElement.checked = false;
+    document.getElementById("fecha").value = "";
+    document.getElementById("numerodetelefono").value = "";
+    document.getElementById("correoeletronico").value = "";
+
+    alert("Información guardada con éxito");
+}
+
+function mostrarAlerta(mensaje) {
+    let alerta = document.getElementById("alert");
+    alerta.textContent = mensaje;
+    alerta.classList.remove("alert2");
+    alerta.classList.add("alert2");
+    setTimeout(() => {
+        alerta.textContent = "";
+        alerta.classList.remove("alert2");
+    }, 1000);
 }
